6f87beb9be3050d428aecfb31ee9ad3e8153fec4
Conecta Lutas <projetositecampeonatos@gmail.com>
2025-05-27 16:27:33 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -126,10 +126,10 @@ app.UseAuthorization(); // Verifica as permissões do usuário autenticado
 app.MapControllers();
 
 //// Aplica automaticamente todas as migrations pendentes ao iniciar a aplicação
-//using (var scope = app.Services.CreateScope())
-//{
-//    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-//    db.Database.Migrate();
-//}
+using (var scope = app.Services.CreateScope())
+{
+   var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+   db.Database.Migrate();
+}
 
 app.Run();
