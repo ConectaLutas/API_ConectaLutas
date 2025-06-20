69b4b77fc94ccbad5a94e41945c261786b5a45aa
Conecta Lutas <projetositecampeonatos@gmail.com>
2025-06-19 22:55:02 -0300

    Update1 Program.cs

--- Program.cs
+++ Program.cs
@@ -10,8 +10,6 @@ using Microsoft.AspNetCore.Http; // Necessário para HttpContext
 using QuestPDF.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-// Defina o tipo de licença do QuestPDF
-QuestPDF.Settings.License = LicenseType.Community;
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 Console.WriteLine($"Connection String: {connectionString}");
 
